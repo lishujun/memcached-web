dd4fe70e6f535f979cae010f931038364dfff38a
lishujun <icoding@qq.com>
2018-05-07 15:25:10 +0800

    RESTful风格改造：修改登录接口

--- user.go
+++ user.go
@@ -7,29 +7,29 @@ import (
 )
 
 
-func Login(req *http.Request, session sessions.Session) string {
+func Login(req *http.Request, session sessions.Session) (int , string) {
 
     username := req.PostFormValue("username")
     password := req.PostFormValue("password")
     if username == "" || password == ""{
-        return responseJSON(false, "用户名密码不能为空")
+        return http.StatusBadRequest, "用户名密码不能为空"
     }
 
     if username != ConfigReader.Username || password != ConfigReader.Password{
         l4g.Debug(ConfigReader.Username + ":" + ConfigReader.Password)
-        return responseJSON(false, "用户名密码不正确")
+        return http.StatusBadRequest, "用户名密码不正确"
     }
 
     session.Set("user", username)
     l4g.Info("user '%s' at '%s' login", username, req.RemoteAddr)
-    return responseJSON(true, "")
+    return http.StatusOK, ""
 }
 
-func Logout(req *http.Request, session sessions.Session) string {
+func Logout(req *http.Request, session sessions.Session) (int, string) {
 
     l4g.Info("user '%s' at '%s' logout", session.Get("user"), req.RemoteAddr)
     session.Delete("user")
-    return responseJSON(true, "")
+    return http.StatusOK, responseJSON(true, "")
 }
 
 func CheckAuth(session sessions.Session) bool {
